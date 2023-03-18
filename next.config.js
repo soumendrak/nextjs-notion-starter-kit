1047919c01fb1b6f79e53dd50760004a8b708f74
soumendrak <soumendra.s@outlook.com>
2023-03-18 22:57:32 +0530

    image optimization disabled

--- next.config.js
+++ next.config.js
@@ -17,6 +17,7 @@ module.exports = withBundleAnalyzer({
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
+    unoptimized: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
   }
 })
